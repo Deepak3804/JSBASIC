cbf3859f6ba31a9864ec96b4326761b9bf395533
Deepak Chaudhary <136905736+Deepak3804@users.noreply.github.com>
2024-01-19 22:41:52 +0530

    Add files via upload

---
+++ index53.js
@@ -0,0 +1,34 @@
+console.log("this is tutorial 53");
+function greet(name,greetText="greetings from javascript") {
+    let name1 = "Name1";
+    console.log(greetText + " " +name);
+    console.log(name + "   is a good boy");
+}
+function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+}
+
+
+let name = "deepak";
+let name1 = "rajan";
+let name2 = "pavan";
+let name3 = "vikash";
+let greetText = "good morning";
+greet(name, greetText);
+greet(name1, greetText);
+greet(name2, greetText);
+//let returnVal =greet(name3);
+//console.log(returnVal)
+let returnVal = sum(1,2,3);
+console.log(returnVal);
+
+
+
+
+//console.log(name + "   is a good boy");
+//console.log(name1 + "   is a good boy");
+//console.log(name2 + "   is a good boy");
+//console.log(name3 + "   is a good boy");
+
+
